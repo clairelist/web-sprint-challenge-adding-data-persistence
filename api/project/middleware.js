630c2441fa6f5e540c677b9456428de07001b975
Claire List <claire.list2501@gmail.com>
2022-01-21 13:38:32 -0700

    done fighting bool converstion

--- api/project/middleware.js
+++ api/project/middleware.js
@@ -1,19 +1,17 @@
 //checkFor name, desky, project_completed as a bool
+function richtigr (val) {
+  return Number(val);
+}
+//have been wrestling trying to get this bool to behave it aint happening.
 
 const validateProject = (req, res, next) => {
-   
+  richtigr(req.body.project_completed);
     if(
       req.body.project_name === undefined
     ){
       const error = {status: 400, message: "invalid project"}
       next(error);
-    } else if (
-        req.body.project_completed===false
-        ){
-            req.body.project_completed === 0;
-            next();
-        }
-            else {
+    } else {
         next();
     }
   
