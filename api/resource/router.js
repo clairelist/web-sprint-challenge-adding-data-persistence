ab4f8bdbc4824030c4eceb9cf76c10536f48390a
Claire List <claire.list2501@gmail.com>
2022-01-21 11:11:36 -0700

    testing

--- api/resource/router.js
+++ api/resource/router.js
@@ -13,6 +13,17 @@ router.get('/', (req, res) => {
         res.status(500).json({ message: 'Failed to retrieve fruits', error: err.message });
       });
   });
+
 //POST
+router.post('/', (req, res) => {
+    Resource.create(req.body)
+      .then(newResource => {
+        res.status(201).json(newResource);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to create that resource!',error:err.message});
+      });
+  });
+  
 
 module.exports = router;
