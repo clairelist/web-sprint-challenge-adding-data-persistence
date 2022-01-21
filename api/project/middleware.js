5cfc560fdf0e3a0d7df9048e5b59dbecec3df920
Claire List <claire.list2501@gmail.com>
2022-01-21 11:40:45 -0700

    silly bugfix

---
+++ api/project/middleware.js
@@ -0,0 +1,21 @@
+//checkFor name, desky, project_completed as a bool
+
+const validateProject = (req, res, next) => {
+    if(
+      req.body.project_name === undefined ||
+      typeof req.body.project_description === undefined ||
+      !req.body.project_name.trim() ||
+      typeof req.body.project_completed !== Boolean ||
+      !req.body.project_completed
+    ){
+      const error = {status: 400, message: "invalid project"}
+      next(error);
+    } else {
+      next();
+    }
+  
+  }
+
+  module.exports = {
+      validateProject
+  }
--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,11 @@
 // build your `/api/resources` router here
 //DATA section
 const Project = require('./model');
+const {validateProject} = require('./middleware');
+//// note to future claire:: I really hope you don't have to encounter that bug again
+//// but if you are looking here it means you DID !
+//// since you exported the function in an object, you have to import it {here} like so, too.
+//// that was nasty...
 const router = require('express').Router();
 
 //GET
@@ -15,15 +20,22 @@ router.get('/', (req, res) => {
   });
 
 //POST
-router.post('/', (req, res) => {
-    Project.create(req.body)
-      .then(newProject => {
-        res.status(201).json(newProject);
-      })
-      .catch(err => {
-        res.status(500).json({ message: 'Failed to create that PROJECT!',error:err.message});
-      });
+router.post('/', validateProject ,(req, res, next) => {
+    const project = req.body
+
+  Project.create(project)
+    .then(project => {
+      res.status(201).json(project)
+    })
+    .catch(next)
   });
   
+  router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      claireAdvice: 'Finding the real error is 90% of the bug fix',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
 
 module.exports = router;
